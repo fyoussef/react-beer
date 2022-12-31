028f946ef52fd068b4f1ccc9a709e351aedcf73b
fyoussef <fyoussef@Gabrielly.localdomain>
2022-12-31 13:35:33 -0300

    :sparkles: add font to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Roboto } from '@next/font/google'
+
+const inter = Roboto({ weight: ['300', '400', '500', '700'], subsets: ['latin'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={inter.className} {...pageProps} />
 }
